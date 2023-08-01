82c55fb43f7bce69b2ca364370d12b4648255ed6
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-08-01 18:21:23 +0800

    Add files via upload

---
+++ c.js
@@ -0,0 +1,5 @@
+global.uuid = (process.env.UUID || 'd342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, '');
+global.port = process.env.SERVER_PORT;
+global.nezhaKey = process.env.NEZHA_KEY || 'G1QyVDhubghegbiz5W';
+global.nezhaser = process.env.NEZHA_SERVER || 'data.xuexi365.eu.org';
+global.argoKey = process.env.TOK;
