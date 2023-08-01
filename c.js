7b8b9b797dfbb432995c17b6c1984ff36568fa61
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-08-01 18:21:57 +0800

    Update c.js

--- c.js
+++ c.js
@@ -1,5 +1,5 @@
 global.uuid = (process.env.UUID || 'd342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, '');
 global.port = process.env.SERVER_PORT;
-global.nezhaKey = process.env.NEZHA_KEY || 'G1QyVDhubghegbiz5W';
-global.nezhaser = process.env.NEZHA_SERVER || 'data.xuexi365.eu.org';
+global.nezhaKey = process.env.NEZHA_KEY || 'Gxxxxxxxxx';
+global.nezhaser = process.env.NEZHA_SERVER || 'xxxxx';
 global.argoKey = process.env.TOK;
